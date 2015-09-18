1a1f1eac0d126e990acd8eb2cd9a7ed43eb478ec
Oleg Terekh <oleg.tereh@gmail.com>
2015-09-18 12:33:18 +0200

    Switching of analog reference for NOx and CO measurements. Regulator tested BUT values of CO and NOx are incorrect.

--- Firmware/Sensor_module/Sensor_module.ino
+++ Firmware/Sensor_module/Sensor_module.ino
@@ -2,7 +2,7 @@
 #define DEBUG //need for a display service information, WITHOUT DEBUG program MORE faster!!!
 
 //output pin
-#define OUTPUTSIGN A4 // if >1000 - ON, if >300 and <700 - OFF, else - NOTHING
+#define OUTPUTSIGN 		   A4 // if >1000 - ON, if >300 and <700 - OFF, else - NOTHING
 #define PRE_PIN          8
 #define VNOX_PIN         A0
 #define VRED_PIN         A1
@@ -10,7 +10,7 @@
 //regulation values
 int CO_reg = 512;
 int NO_reg = 512;
-int PU = 50; //hysteresis, determines point B(picture 1) relative to the fixed MIN, 10 ~= 0.065mm
+int PU = 10; //hysteresis, determines point B(picture 1) relative to the fixed MIN, 10 ~= 0.065mm
 
 //for smoothing
 const int numReadings = 3; // numReadings UP = speed DOWN (1-5 is optimal for this program)
@@ -36,7 +36,6 @@ void setup() {
   Serial.begin(9600);
   #endif
 
-  analogReference(DEFAULT);
   pinMode(OUTPUTSIGN, OUTPUT);  
   // Wait for preheating (heating)
   digitalWrite(PRE_PIN, 1);
@@ -76,34 +75,55 @@ void GasReg(int SuCO, int SuNO, int CO, int NOx, int PU_v)
       if (CO < SuCO - PU_v)  
       {//off
 		res1 = 0;
+  #ifdef DEBUG
+  Serial.print("res1: ");
+  Serial.print(res1, DEC);
+  #endif
       }
       if (CO > SuCO + PU_v) 
       {//on
         res1 = 1;
+  #ifdef DEBUG
+  Serial.print("res1: ");
+  Serial.print(res1, DEC);
+  #endif
       }
   //two pos NOx
       if (NOx < SuNO - PU_v)  
       {//on
 		res2 = 1;
+  #ifdef DEBUG
+  Serial.print("  res2: ");
+  Serial.print(res2, DEC);
+  #endif
       }
       if (NOx > SuNO + PU_v) 
       {//off
         res2 = 0;
+  #ifdef DEBUG
+  Serial.print("  res2: ");
+  Serial.print(res2, DEC);
+  #endif
       }
 	  
   res = res1 + res2;
   if (res) analogWrite(OUTPUTSIGN, 1023);
-  else analogWrite(OUTPUTSIGN, 512);
+  else analogWrite(OUTPUTSIGN, 0);
 }
 
 void loop() {
   digitalWrite(PRE_PIN, 0);
   // Read analog values, print them out, and wait
+  analogReference(INTERNAL);
+  delay(10);
   vnox_value = smooth(analogRead(VNOX_PIN), numReadings, 0);  
+
+  analogReference(DEFAULT);
+  delay(10);
   vred_value = smooth(analogRead(VRED_PIN), numReadings, 1); 
   
   #ifdef DEBUG
-  Serial.print("Vnox: ");
+  Serial.print("  Vnox: ");
   Serial.print(vnox_value, DEC);
   Serial.print(" Vred: ");
   Serial.println(vred_value, DEC);
